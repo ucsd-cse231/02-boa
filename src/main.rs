8438f6798af612f234ddbf616340bd2dc2c21e2e
Ranjit Jhala <jhala@cs.ucsd.edu>
2024-03-26 15:17:52 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+use sexp::Atom::*;
+use sexp::*;
+
+use im::HashMap;
+
+#[derive(Debug)]
+enum Val {
+    Reg(Reg),
+    Imm(i32),
+    RegOffset(Reg, i32),
+}
+
+#[derive(Debug)]
+enum Reg {
+    RAX,
+    RSP,
+}
+
+#[derive(Debug)]
+enum Instr {
+    IMov(Val, Val),
+    IAdd(Val, Val),
+    ISub(Val, Val),
+}
+
+#[derive(Debug)]
+enum Op1 {
+    Add1,
+    Sub1,
+}
+
+#[derive(Debug)]
+enum Op2 {
+    Plus,
+    Minus,
+    Times,
+}
+
+#[derive(Debug)]
+enum Expr {
+    Number(i32),
+    Id(String),
+    Let(Vec<(String, Expr)>, Box<Expr>),
+    UnOp(Op1, Box<Expr>),
+    BinOp(Op2, Box<Expr>, Box<Expr>),
+}
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let in_name = &args[1];
+    let out_name = &args[2];
+
+    // You will make result hold the result of actually compiling
+    let result = "mov rax, 131";
+
+    let asm_program = format!(
+        "
+section .text
+global our_code_starts_here
+our_code_starts_here:
+  {}
+  ret
+",
+        result
+    );
+
+    let mut out_file = File::create(out_name)?;
+    out_file.write_all(asm_program.as_bytes())?;
+
+    Ok(())
+}
